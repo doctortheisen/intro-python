c33c4895dea6d0328900ae7dae376577e3b0f881
B. Theisen <17404276+doctortheisen@users.noreply.github.com>
2021-04-06 21:11:16 -0700

    Update responses.py

--- assignments/w03/responses.py
+++ assignments/w03/responses.py
@@ -1,4 +1,13 @@
 # responses.py
-import pandas as pd
-responses = pd.Series(["Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Disagree"], dtype="category")
-responses
+#   usage:      example code for coercing python into acting like R for factors(levels))
+#   
+#   B. Theisen wrote this code on 4/6/2021, based on tutorial below:
+# 
+#   https://pandas.pydata.org/pandas-docs/stable/user_guide/categorical.html#sorting-and-order
+# 
+# 
+# import pandas as pd
+responses = pd.Series(["Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Agree"], dtype="category")
+#responses = pd.Series(["Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Agree"]).astype(CategoricalDtype(ordered=True))
+responses = responses.cat.set_categories(["Strongly Disagree", "Disagree", "Neither Agree nor Disagree", "Agree", "Strongly Agree"])
+print(responses)
