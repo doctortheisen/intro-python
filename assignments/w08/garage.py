d5654c569709c78909c51decfdc4865e76002685
B. Theisen <17404276+doctortheisen@users.noreply.github.com>
2021-05-09 12:42:53 -0700

    working but incomplete version of garage program

--- assignments/w08/garage.py
+++ assignments/w08/garage.py
@@ -4,27 +4,30 @@
 #
 from vehicle import Vehicle, Car, Truck
 #
-# Car test
-#new_car = Car('toyota', 'camry', 'gray', 'unleaded', options_list[0].options, '4-cylinder', 4)
-#print(new_car.getMake(), new_car.getModel(), new_car.getColor(), new_car.getFuelType(), new_car.getOptions())
-#
-# Truck test
-#new_truck = Truck('toyota', 'tacoma', 'green', 'unleaded', 'power mirrors', 'supercab', 'short')
-#print(new_truck.getMake(), new_truck.getModel(), new_truck.getColor(), new_truck.getFuelType(), new_truck.getOptions())
-
-# 2. Demonstrate usage of previously studied programming constructs (functions, conditionals, loops)  - 60%
-# Using a function, display a menu prompting the user to add a Car or a Pickup to their virtual garage. - 15%
 num_cars = 0
 num_trucks = 0
 
-# Add car menu
-def add_car():
-#    cars = {}
-    adding_car_state = True
-
-    while adding_car_state:
-        new_car = Car(input('Make? '), input('Model? '), input('Color? '), input('Fuel Type? '), input('Options? '), input('Engine Size? '), input('Number of Doors? '))
-    
+# Add vehicles menu
+def add_vehicle_menu():
+    global num_cars
+    global num_trucks
+    while(True):
+        new_vehicle = input('Welcome to Garage Main Menu. Which type vehicle would you like to add to your garage? (Type car, truck, or exit)\n').lower()
+            # exit loop if user inputs exit request
+        if new_vehicle == 'exit':
+            break
+        elif new_vehicle == 'car':
+#           cars = []
+            new_car = Car(
+                input('Make? '),
+                input('Model? '),
+                input('Color? '),
+                input('Fuel Type? '),
+                input('Options? '),
+                input('Engine Size? '),
+                input('Number of Doors? '))
+            # How do i store the new_car instance to a list?
+            # How do I print from that list?
 #        print(
 #            'Your garage has a new car:\n', 
 #            '\nMake: ', new_car.getMake(),
@@ -34,51 +37,19 @@ def add_car():
 #            '\n: Options: ', new_car.getOptions(),
 #            '\n: Engine Size: ', new_car.getEngineSize(),
 #            '\n: Number of Doors: ', new_car.getNumDoors()
-#            )
-#    cars[] = new_car
-    adding_car_state = False
-    return new_car
 
-# Add truck menu
-def add_truck():
-    adding_truck_state = True
-
-    while adding_truck_state:
-    
-        new_truck = Truck(input('Make? '), input('Model? '), input('Color? '), input('Fuel Type? '), input('Options? '), input('Cab Style? '), input('Bed Length? '))
-    
-#    print(
-#        'Your garage has a new car:\n', 
-#        '\nMake: ', new_truck.getMake(),
-#        '\nModel: ', new_truck.getModel(),
-#        '\nColor: ', new_truck.getColor(),
-#        '\n: Fuel Type: ', new_truck.getFuelType(),
-#        '\n: Options: ', new_truck.getOptions(),
-#        '\n: Engine Size: ', new_truck.getCabStyle(),
-#        '\n: Number of Doors: ', new_truck.getBedLength()
-#        )
-        another_truck = input('\nAdd another truck? yes or no: ') 
-        if  another_truck == 'no':
-            adding_truck_state = False
-        #elif another_truck == 'yes':
-        #    adding_truck_state = 
-    return new_truck
-
-# Add vehicles menu
-def add_vehicle_menu():
-    global num_cars
-    global num_trucks
-    while(True):
-        new_vehicle = input('What vehicle do you want to add to your garage? (Type car, truck, or exit)\n').lower()
-            # exit loop if user inputs exit request
-        if new_vehicle == 'exit':
-            break
-        elif new_vehicle == 'car':
-            add_car()
+#           cars = [ Car( make, model, color, fuelType, options, engineSize, numDoors ) for make, model, color, fuelType, options, engineSize, numDoors in new_car ]
             num_cars = num_cars + 1
             print(num_cars, 'car(s) in garage.\n')
         elif new_vehicle == 'truck':
-            add_truck()
+            new_truck = Car(
+                input('Make? '),
+                input('Model? '),
+                input('Color? '),
+                input('Fuel Type? '),
+                input('Options? '),
+                input('Cab Style? '),
+                input('Bed Length? '))
             num_trucks = num_trucks + 1
             print(num_trucks, 'truck(s) in garage.\n')
         else:
