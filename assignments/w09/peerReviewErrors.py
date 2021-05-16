e3e87f45a84a08cb5ea042ec7de76fb809a8eb8d
B. Theisen <17404276+doctortheisen@users.noreply.github.com>
2021-05-15 22:44:26 -0700

    Update peerReviewErrors.py

--- assignments/w09/peerReviewErrors.py
+++ assignments/w09/peerReviewErrors.py
@@ -1,8 +1,8 @@
 # This is a header for the application
 # You should read this header and insert your name and your date below as part of the peer review
 # This is a typical part of any program
-# Author: <author>
-# Creation Date: <date>
+# Author: Benjamin Theisen
+# Creation Date: 05/15/2021
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
 
 import random
@@ -16,12 +16,11 @@ def displayIntro():
 	print()
 
 def chooseCave():
-    cave = ''
-	while cave != '1' and cave != '2':
+	cave = ''
+	while (cave != '1') and (cave != '2'):
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
-
-	return caves
+	return cave
 
 def checkCave(chosenCave):
 	print('You approach the cave...')
@@ -29,26 +28,26 @@ def checkCave(chosenCave):
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-	time.sleep(3)
+	time.sleep(2)
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
 	time.sleep(2)
 	friendlyCave = random.randint(1, 2)
+	print({friendlyCave}, {chosenCave})
 
-	if chosenCave == str(friendlyCave):
+	if int(chosenCave) == friendlyCave:
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+		print('Gobbles you down in one bite!')
 
 playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
+while (playAgain == 'yes') or (playAgain == 'y'):
 	displayIntro()
-	caveNumber = choosecave()
+	caveNumber = chooseCave()
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
 	if playAgain == "no":
-		print("Thanks for planing")
-
+		print("Thanks for playing!")
